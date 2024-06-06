b4097921632ee8fe3d6f642d0f4cc0a5efcfb5fb
Mamabolo Katlego <rabotaikatlego@gmail.com>
2024-06-06 13:07:53 +0000

    all files

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module Docs
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers and returns the result.
+
+    Parameters:
+    - a (float): The first float operand.
+    - b (float): The second float operand.
+
+    Returns:
+    float: The sum of a and b.
+    """
+    result: float = a + b
+    return result
